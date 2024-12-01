24578ffd42fb7ea027783e2b17ee476984dad96e
Eb0b0l0 <89797412+ebobo54@users.noreply.github.com>
2024-12-01 18:54:03 +0500

    1

--- lab_1/lab_1.c
+++ lab_1/lab_1.c
@@ -1,61 +1,144 @@
 
-#include <stdlib.h>
+// #include <stdlib.h>
+// #include <stdio.h>
+
+// struct item {
+//   int data;
+//   struct item *next;
+// };
+
+// void print_list(const struct item *lst)
+// {
+//   printf("list: ");
+//   for(; lst; lst = lst->next)
+//     printf("%d ", lst->data);
+//   printf("\n");
+// }
+
+// struct item *array_to_list(int *arr, int size)
+// {
+//   struct item *tmp;
+//   if(!size)
+//     return NULL;
+//   tmp = malloc(sizeof(struct item));
+//   tmp->data = *arr;
+//   tmp->next = array_to_list(arr + 1, size - 1);
+//   return tmp;
+// }
+
+
+// void delete_element_list(struct item **pcur, int num)
+// {
+//   int counter = 1;
+//   while(*pcur) {  
+//     if(num == counter) {
+//       struct item *tmp = *pcur; 
+//       *pcur = (*pcur)->next;
+//       free(tmp);
+//       break;
+//     } else {
+//         pcur = &(*pcur)->next;
+//         counter++;
+//     }
+//   }
+// }
+
+// int main()
+// {
+//   int arr[] = {1, -2, 3, -4, 5};
+//   int size = sizeof(arr) / sizeof(*arr);
+//   struct item *lst = array_to_list(arr, size);
+  
+//   print_list(lst);
+  
+//   delete_element_list(&lst, 1);
+//   print_list(lst);
+  
+//   delete_element_list(&lst, 3);
+//   print_list(lst);
+  
+//   return 0;
+// }
 #include <stdio.h>
+#include <stdlib.h>
 
-struct item {
-  int data;
-  struct item *next;
-};
+// Структура узла односвязного списка
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
 
-void print_list(const struct item *lst)
-{
-  printf("list: ");
-  for(; lst; lst = lst->next)
-    printf("%d ", lst->data);
-  printf("\n");
-}
-
-struct item *array_to_list(int *arr, int size)
-{
-  struct item *tmp;
-  if(!size)
-    return NULL;
-  tmp = malloc(sizeof(struct item));
-  tmp->data = *arr;
-  tmp->next = array_to_list(arr + 1, size - 1);
-  return tmp;
-}
-
-
-void delete_element_list(struct item **pcur, int num)
-{
-  int counter = 1;
-  while(*pcur) {  
-    if(num == counter) {
-      struct item *tmp = *pcur; 
-      *pcur = (*pcur)->next;
-      free(tmp);
-      break;
-    } else {
-        pcur = &(*pcur)->next;
-        counter++;
+// Функция для создания нового узла
+Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if (!newNode) {
+        printf("Ошибка выделения памяти\n");
+        exit(1);
     }
-  }
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
 }
 
-int main()
-{
-  int arr[] = {1, -2, 3, -4, 5};
-  int size = sizeof(arr) / sizeof(*arr);
-  struct item *lst = array_to_list(arr, size);
-  
-  print_list(lst);
-  
-  delete_element_list(&lst, 1);
-  print_list(lst);
-  
-  delete_element_list(&lst, 3);
-  print_list(lst);
-  
-  return 0;
+void insertBefore(Node** head, int index, int newData) {
+    if (index < 0) {
+        printf("Индекс должен быть >= 0\n");
+        return;
+    }
+
+    Node* newNode = createNode(newData);
+
+    if (index == 0) {
+        newNode->next = *head;
+        *head = newNode;
+        return;
+    }
+
+    Node* current = *head;
+    Node* prev = NULL;
+    int currentIndex = 0;
+
+    while (current != NULL && currentIndex < index) {
+        prev = current;
+        current = current->next;
+        currentIndex++;
+    }
+
+    if (currentIndex != index) {
+        printf("Индекс вне границ списка\n");
+        free(newNode);
+        return;
+    }
+
+    prev->next = newNode;
+    newNode->next = current;
+}
+
+void printList(Node* head) {
+    Node* temp = head;
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    Node* head = NULL;
+
+    head = createNode(10);
+    head->next = createNode(20);
+    head->next->next = createNode(30);
+
+    printf("list: ");
+    printList(head);
+
+    insertBefore(&head, 1, 15);
+    printf("15 before index 1: ");
+    printList(head);
+
+    insertBefore(&head, 0, 5);
+    printf("5 before index 0: ");
+    printList(head);
+    return 0;
 }
--- lab_1/list.c
+++ lab_1/list.c
@@ -58,64 +58,137 @@
   
 //   return 0;
 // }
+// #include <stdlib.h>
+// #include <stdio.h>
+
+// struct node {
+//     int val;
+//     struct node *left, *right;
+// };
+
+// void bin_tree_print(struct node *root)
+// {
+//     if(!root)
+//        return;
+//      bin_tree_print(root->left);
+//      printf("  %d   ", root->val);
+//      bin_tree_print(root->right);    
+// }    
+
+// void bin_tree_add(struct node **root, int val)
+// {
+//     if(!*root) {
+//         *root = malloc(sizeof(struct node));
+//         (*root)->val = val;
+//         (*root)->left = NULL;
+//         (*root)->right = NULL;
+//         return;
+//     }
+//     if((*root)->val == val)
+//         return;
+//     if(val < (*root)->val)
+//         bin_tree_add(&(*root)->left, val);
+//     else
+//         bin_tree_add(&(*root)->right, val);
+// }    
+
+// int main()
+// {  
+//    struct node *root = NULL;
+//    bin_tree_add(&root, 27);
+//    bin_tree_print(root);
+//    printf("\n");
+//    bin_tree_add(&root, 18);
+//    bin_tree_print(root);
+//    printf("\n");   
+//    bin_tree_add(&root, 34);
+//    bin_tree_print(root);
+//    printf("\n");   
+//    bin_tree_add(&root, 4);
+//    bin_tree_print(root);
+//    printf("\n");   
+//    bin_tree_add(&root, 21);
+//    bin_tree_print(root);
+//    printf("\n");   
+//    bin_tree_add(&root, 29);
+//    bin_tree_print(root);
+//    printf("\n");   
+//    bin_tree_add(&root, 39);
+//    bin_tree_print(root);
+//    printf("\n");
+//    return 0;   
+// }    
+
 #include <stdlib.h>
 #include <stdio.h>
 
-struct node {
-    int val;
-    struct node *left, *right;
+struct item {
+  int data;
+  struct item *next;
 };
 
-void bin_tree_print(struct node *root)
+void print_list(const struct item *lst)
 {
-    if(!root)
-       return;
-     bin_tree_print(root->left);
-     printf("  %d   ", root->val);
-     bin_tree_print(root->right);    
-}    
+  printf("list: ");
+  for(; lst; lst = lst->next)
+    printf("%d ", lst->data);
+  printf("\n");
+}
 
-void bin_tree_add(struct node **root, int val)
+struct item *array_to_list(int *arr, int size)
 {
-    if(!*root) {
-        *root = malloc(sizeof(struct node));
-        (*root)->val = val;
-        (*root)->left = NULL;
-        (*root)->right = NULL;
-        return;
+  struct item *tmp;
+  if(!size)
+    return NULL;
+  tmp = malloc(sizeof(struct item));
+  tmp->data = *arr;
+  tmp->next = array_to_list(arr + 1, size - 1);
+  return tmp;
+}
+
+
+void delete_item_list(struct item **pcur, int num)
+{
+  int counter = 1;
+  while(*pcur) {  
+    if(num == counter) {
+      struct item *tmp = *pcur;
+      *pcur = (*pcur)->next;
+      free(tmp);
+      break;
+    } else {
+        pcur = &(*pcur)->next;
+        counter++;
     }
-    if((*root)->val == val)
-        return;
-    if(val < (*root)->val)
-        bin_tree_add(&(*root)->left, val);
-    else
-        bin_tree_add(&(*root)->right, val);
-}    
+  }
+}
+
+void insert_item_list(struct item **pcur, int index, int data)
+{
+  int counter = 1;
+
+  while(*pcur) {  
+    if(index == counter) {
+      struct item *tmp = malloc(sizeof(struct item));
+      tmp->data = data;
+      tmp->next = *pcur;
+      *pcur = tmp;
+      break;
+    } else {
+        pcur = &(*pcur)->next;
+        counter++;
+    }
+  }
+}
+
 
 int main()
-{  
-   struct node *root = NULL;
-   bin_tree_add(&root, 27);
-   bin_tree_print(root);
-   printf("\n");
-   bin_tree_add(&root, 18);
-   bin_tree_print(root);
-   printf("\n");   
-   bin_tree_add(&root, 34);
-   bin_tree_print(root);
-   printf("\n");   
-   bin_tree_add(&root, 4);
-   bin_tree_print(root);
-   printf("\n");   
-   bin_tree_add(&root, 21);
-   bin_tree_print(root);
-   printf("\n");   
-   bin_tree_add(&root, 29);
-   bin_tree_print(root);
-   printf("\n");   
-   bin_tree_add(&root, 39);
-   bin_tree_print(root);
-   printf("\n");
-   return 0;   
-}    
-
+{
+  int arr[] = {1, 2, 3};
+  int size = sizeof(arr) / sizeof(*arr);
+  struct item *lst = array_to_list(arr, size); 
+  print_list(lst);
+  insert_item_list(&lst, 3, 9);
+  print_list(lst);
+  return 0;
+}
